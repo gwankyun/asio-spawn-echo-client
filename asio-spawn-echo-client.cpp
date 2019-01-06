67d1dad1edfd5dcef4cf5fb12c60929858a66e58
權少 <ljqican@163.com>
2019-01-06 23:47:32 +0800

    完全重構，修正之前的所有問題

--- asio-spawn-echo-client.cpp
+++ asio-spawn-echo-client.cpp
@@ -1,61 +1,181 @@
-﻿//#include "pch.h"
-#include "asio-spawn-echo-client.h"
+﻿// asio-spawn-echo-server.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
 
-session_t::session_t(io_context_t &io_context)
-	: socket(io_context)
+#include "pch.h"
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <functional>
+#include <numeric>
+
+#include <cmath>
+
+#include <boost/asio.hpp>
+#include <boost/asio/spawn.hpp>
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog_easy.hpp>
+
+#define NOMINMAX
+
+using namespace std;
+
+namespace asio = boost::asio;
+
+using io_context_t = asio::io_context;
+using tcp_t = asio::ip::tcp;
+using socket_t = tcp_t::socket;
+using address_t = asio::ip::address;
+using endpoint_t = tcp_t::endpoint;
+using acceptor_t = tcp_t::acceptor;
+using resolver_t = tcp_t::resolver;
+using error_code_t = boost::system::error_code;
+using yield_context_t = asio::yield_context;
+
+class session_t : public enable_shared_from_this<session_t>
 {
+public:
+	session_t() = default;
+	session_t(shared_ptr<socket_t> socket_);
+	session_t(io_context_t &io_context);
+	~session_t();
+
+	void run();
+	string address();
+	uint16_t port();
+
+	shared_ptr<socket_t> socket;
+
+private:
+
+};
+
+string session_t::address()
+{
+	return socket->remote_endpoint().address().to_string();
+}
+
+uint16_t session_t::port()
+{
+	return socket->remote_endpoint().port();
 }
 
 session_t::~session_t()
 {
-	INFO("log");
+	LOG_INFO("log", "address:{0} port:{1}", address(), port());
 }
 
-void session_t::go()
+session_t::session_t(shared_ptr<socket_t> socket_)
+{
+	socket = socket_;
+}
+
+session_t::session_t(io_context_t &io_context)
+{
+	socket = make_shared<socket_t>(io_context);
+}
+
+template<typename Pred>
+error_code_t read_util(socket_t &socket, vector<char> &buffer, std::size_t &read_offset, yield_context_t &yield, Pred pred)
+{
+	error_code_t ec;
+	while (true)
+	{
+		buffer.resize(read_offset + 2);
+		auto read_size = socket.async_read_some(
+			asio::buffer(buffer.data() + read_offset, 2), yield[ec]);
+		if (ec)
+		{
+			LOG_ERROR("log", ec.message());
+			return ec;
+		}
+
+		LOG_DEBUG("log", "read_size:{0}", read_size);
+
+		read_offset += read_size;
+
+		if (pred())
+		{
+			LOG_INFO("log", "message:{0}", buffer.data());
+			return ec;
+		}
+	}
+}
+
+error_code_t write_n(socket_t &socket, const vector<char> &buffer, std::size_t read_offset, yield_context_t &yield)
+{
+	error_code_t ec;
+	std::size_t write_offset = 0;
+	while (true)
+	{
+		std::size_t size = std::min(read_offset - write_offset, std::size_t(2));
+		LOG_DEBUG("log", "size:{0}", size);
+		auto write_size = socket.async_write_some(
+			asio::buffer(buffer.data() + write_offset, size), yield[ec]);
+
+		if (ec)
+		{
+			return ec;
+		}
+
+		write_offset += write_size;
+
+		LOG_DEBUG("log", "write_size:{0}", write_size);
+
+		LOG_DEBUG("log", "write_offset:{0} read_offset:{1}",
+			write_offset,
+			read_offset);
+
+		if (write_offset == read_offset)
+		{
+			return ec;
+		}
+	}
+}
+
+void session_t::run()
 {
 	auto self(shared_from_this());
-	asio::spawn(socket.get_io_context(),
+	asio::spawn(socket->get_io_context(),
 		[this, self](yield_context_t yield)
 	{
-		string message;
-		while (std::getline(cin, message))
+		error_code_t ec;
+		while (true)
 		{
-			INFO("log", message);
-			error_code_t ec;
-			auto size = socket.async_write_some(asio::buffer(message), yield[ec]);
+			string message;
+			vector<char> buffer;
+			if (std::getline(cin, message))
+			{
+				LOG_INFO("log", "getline message:{0}", message);
+				copy_n(message.begin(), message.size(), back_inserter(buffer));
+				buffer.push_back('\0');
+			}
 
+			ec = write_n(*socket, buffer, buffer.size(), yield);
 			if (ec)
 			{
-				INFO("log", ec.message());
+				LOG_ERROR("log", ec.message());
+				return;
 			}
-			else
+
+			buffer.clear();
+
+			std::size_t read_offset = 0;
+			ec = read_util(*socket, buffer, read_offset, yield,
+				[&buffer, &read_offset]()
 			{
-				INFO("log", "async_write_some:{0}", size);
+				return buffer[read_offset - 1] == '\0';
+			});
 
-				vector<char> buff(1024, '\0');
-
-				while (true)
-				{
-					auto read_size = socket.async_read_some(asio::buffer(buff), yield[ec]);
-					if (ec)
-					{
-						ERROR("log", ec.message());
-						exit(1);
-					}
-					else
-					{
-						copy_n(buff.begin(), read_size, back_inserter(buffer));
-						if (read_size < buff.size())
-						{
-							break;
-						}
-					}
-				}
-
-				buffer.push_back('\0');
-				INFO("log", "async_read_some:{0}", buffer.data());
-				buffer.clear();
-			}
+			LOG_INFO("log", "address:{0} ip:{1} server message:{2}",
+				address(),
+				port(),
+				buffer.data());
 		}
 	});
 }
@@ -63,28 +183,34 @@ void session_t::go()
 int main()
 {
 	auto logger = spdlog::stdout_color_mt("log");
+	spdlog::set_level(spdlog::level::level_enum::debug);
 	io_context_t io_context;
-
 	asio::spawn(io_context,
-		[&io_context](yield_context_t yield)
+		[&](yield_context_t yield)
 	{
-		error_code_t ec;
+		acceptor_t acceptor(io_context, endpoint_t(address_t::from_string("0.0.0.0"), 12500));
 
-		auto session = make_shared<session_t>(io_context);
-		auto &socket = session->socket;
+		//while (true)
+		{
+			error_code_t ec;
+			auto session = make_shared<session_t>(io_context);
 
-		socket.async_connect(endpoint_t(address_t::from_string("127.0.0.1"), 12500), yield[ec]);
-		if (ec)
-		{
-			ERROR("log", ec.message());
-			exit(1);
-		}
-		else
-		{
-			INFO("log", "address:{0} port:{1}", session->address, session->port);
-			session->go();
+			session->socket->async_connect(
+				endpoint_t(address_t::from_string("127.0.0.1"), 12500), yield[ec]);
+
+			if (ec)
+			{
+				LOG_ERROR("log", ec.message());
+				exit(1);
+			}
+
+			LOG_INFO("log", "address:{0} port:{1}",
+				session->address(),
+				session->port());
+
+			session->run();
 		}
 	});
-
 	io_context.run();
+	return 0;
 }
